b0b4d1d9a68cd4fa8d1d0e1dac09408af91ea6fa
KeynnerDFS <147161794+KeynnerDFS@users.noreply.github.com>
2024-08-01 09:56:00 -0400

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEscola.RegarasDeNegocio
+{
+    internal class Aluno
+    {
+        //Atributos com mátodos GET e SET automatizados
+        public int Id { get; set; }
+        public int NumeroDeMatricula { get; set; }
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+
+        //Métodos
+        public double CalcularMedia()
+        {
+            return (Nota1 + Nota2) / 2;
+        }
+        public string MostrarSituação()
+        {
+            string situação = "REPROVADO(A)";
+            if (CalcularMedia() >= 60)
+            {
+                situação = "APROVADO(A)";
+            }
+            return situação;
+        }
+
+    }
+}
---
+++ AppEscola.cs
@@ -0,0 +1,139 @@
+﻿using AppEscola.RegarasDeNegocio;
+
+Aluno[] salaDeAula = new Aluno[350];
+
+int cont = 1;
+int posição = 0;
+int opção = 0;
+//############################# Função para cadastrar aluno ############################
+
+void CadastarAlunos()
+{
+    int opc = 1;
+    while (opc != 0)
+    {
+        if (cont < 350)
+        {
+            Aluno aluno = new Aluno();
+            Console.Clear();
+            Console.WriteLine("%%%%%%%%%%%%%%% CADASTRAR ALUNO %%%%%%%%%%%%%%%");
+            Console.WriteLine();
+            Console.WriteLine("Número de Inscrição.......:"+cont);
+            aluno.Id = cont;
+            aluno.NumeroDeMatricula = cont * 1000;
+            Console.Write("Nome................:");
+            aluno.Nome = Console.ReadLine();
+            Console.Write("Nota1...............:");
+            aluno.Nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nota1...............:");
+            aluno.Nota2 = Convert.ToDouble(Console.ReadLine());
+            salaDeAula[posição] = aluno;
+            cont++; posição++;
+            Console.Write("Deseja continuar cadastrando? S/N:");
+            string resp = Console.ReadLine();
+            if (resp.ToUpper() == "N") opc = 0;
+          
+        }
+        else
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("VAGAS ESGOTADAS!!!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }//fim do if
+    }//fim do while
+}//fim cadastrar aluno
+void ListarAlunosMatriculados()
+{
+    Console.Clear();
+    Console.WriteLine("@@@@@@@@@@@@@@@@@@ LISTA DOS ALUNOS CADASTRADOS @@@@@@@@@@@@@@@@");
+    Console.WriteLine();
+    Aluno aluno = new Aluno();
+    for (int i = 0; i < posição; i++)
+    {
+        aluno = salaDeAula[i];
+        Console.WriteLine("_____________________________________");
+        Console.WriteLine("Id....................!" + aluno.Id);
+        Console.WriteLine("Matrícula.............!" + aluno.NumeroDeMatricula);
+        Console.WriteLine("Nome..................!" + aluno.Nome);
+        Console.WriteLine("Nota1.................!" + aluno.Nota1);
+        Console.WriteLine("Nota2.................!" + aluno.Nota2);
+        Console.WriteLine("Média.................!" + aluno.CalcularMedia());
+        if (aluno.CalcularMedia() < 60) Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Situação..............!" + aluno.MostrarSituação());
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+
+    Console.ReadKey();
+
+}//fim de listar alunos
+void ConsultarAlunos()
+{
+    Aluno aluno = new Aluno();
+
+
+}//Fim de consultar aluno
+while (opção!=7)
+{
+    Console.Clear();//Limpar tela
+    Console.WriteLine("@@@@@@@@@@ MENU @@@@@@@@@@");
+    Console.WriteLine();
+    Console.WriteLine("1 - Cadastrar aluno");
+    Console.WriteLine("2 - Listar alunos matrículados");
+    Console.WriteLine("3 - Consultar aluno por Número de Matrícula");
+    Console.WriteLine("4 - Listar alunos aprovados");
+    Console.WriteLine("5 - Listar alunos Reprovados");
+    Console.WriteLine("7 - Fechar Sistema");
+    Console.Write("Opção N°......................................:");
+    opção = Convert.ToInt32(Console.ReadLine());
+    switch (opção)
+    {
+        case 1:
+            {
+                CadastarAlunos();
+                break;
+            }//fim do case 1
+        case 2:
+            {
+                ListarAlunosMatriculados();
+                break;
+            }//fim do case 2
+        case 3:
+            {
+
+                break;
+            }//fim do case 3
+        case 4:
+            {
+
+                break;
+            }//fim do case 4
+        case 5:
+            {
+
+                break;
+            }//fim do case 5
+        case 7:
+            {
+                Console.Clear();
+                Console.WriteLine("Desaja mesmo Fechar o SISTEMA? S/N");
+                string resposta = Console.ReadLine();
+                if (resposta.ToUpper() == "N") opção = 0;
+                break;
+            }//fim do case 7
+        default:
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("OPÇÃO INVÁLIDA");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                break;
+            }//Fim do default
+    }//fim do switch
+
+
+
+}//fim do laço principal
